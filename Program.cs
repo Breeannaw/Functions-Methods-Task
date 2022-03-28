37b689104ef5e69f101fdcb88d562aba2b6d86fc
BREEANNA WHITE <104002563@student.swin.edu.au>
2022-03-29 09:44:00 +1100

    task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Functions_Methods_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
